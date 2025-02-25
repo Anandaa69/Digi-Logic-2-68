c49a3c0a79897c21ef88988874e2fe6b0e2b65f2
Anandaa69 <anandasinlapachote0@gmail.com>
2025-02-25 20:45:38 +0700

    Update PWM.ino

--- PWM.ino
+++ PWM.ino
@@ -1,17 +1,16 @@
-const int ledPin = 32; /* LED pin */
+const int ledPin = 32;
 const int ledChannel = 0;
 const int resolution = 8;
 const int freq = 10;
-const int maxDuty = 255; //2^resolution-1
+const int maxDuty = 255;
 void setup() {
   Serial.begin(115200);
   ledcSetup(ledChannel, freq, resolution);
-  ledcAttachPin(ledPin, ledChannel); // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
 
 void loop() {
   for(int dutyCycle = 0; dutyCycle <= maxDuty; dutyCycle++){   
-    // changing the LED brightness with PWM
     ledcWrite(ledChannel, dutyCycle);
     Serial.println(dutyCycle);
     delay(30);
